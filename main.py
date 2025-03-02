58fbdd167e3028ca3b233eca2311667c86575c46
kikofers <dolelext@gmail.com>
2025-03-02 16:09:44 +0200

    Pielikta komandrinda main.py failā.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from gui.window import MainWindow
 from PyQt5.QtWidgets import QApplication
 import sys
 
+# pyinstaller BITUS_Software.spec
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
